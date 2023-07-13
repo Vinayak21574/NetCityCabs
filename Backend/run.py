5a302256f4f2ed57d27004822b3f4a7a5323a52f
Vinayak21574 <vinayak21574@iiitd.ac.in>
2023-07-13 15:21:22 +0530

    Updates_1

--- Backend/run.py
+++ Backend/run.py
@@ -3,6 +3,8 @@ from main import app
 
 # npm run build
 # Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
+# .\env\Scripts\activate
+# pip install -r .\requirements.txt
 # serve -s build
 # http://192.168.45.245:80
 
